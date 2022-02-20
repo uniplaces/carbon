202dba43a48f050804139b288edc99e922ef95c2
ashwek <ashwekswamy@protonmail.com>
2022-02-20 12:17:13 +0530

    added null checks to NewCarbonInterval

--- carboninterval.go
+++ carboninterval.go
@@ -7,18 +7,25 @@ import (
 // CarbonInterval represents an interval between two carbons.
 type CarbonInterval struct {
 	Start *Carbon
-	End *Carbon
+	End   *Carbon
 }
 
 // NewCarbonInterval returns a pointer to a new CarbonInterval instance
 func NewCarbonInterval(start, end *Carbon) (*CarbonInterval, error) {
+	if start == nil {
+		return nil, errors.New("start cannot be nil")
+	}
+	if end == nil {
+		return nil, errors.New("end cannot be nil")
+	}
+
 	if start.Gte(end) {
-		return nil, errors.New("The end date must be greater than the start date.")
+		return nil, errors.New("the end date must be greater than or equal to the start date")
 	}
 
 	return &CarbonInterval{
 		Start: start,
-		End: end,
+		End:   end,
 	}, nil
 }
 
