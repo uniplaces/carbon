cacaeb006f61e882c1a763a06f4133e9f38748fe
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2024-01-17 10:05:26 +0530

    added null checks to NewCarbonInterval (#73)
    * added null checks to NewCarbonInterval
    
    * added missing test cases for NewCarbonInterval
    
    ---------
    
    Co-authored-by: Renato Duarte <renato.duarte@uniplaces.com>

--- carboninterval_test.go
+++ carboninterval_test.go
@@ -3,6 +3,7 @@ package carbon
 import (
 	"testing"
 	"time"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -11,7 +12,7 @@ func TestErrorOnConstruction(t *testing.T) {
 	endDate, _ := Create(2009, time.November, 9, 23, 0, 0, 0, "UTC")
 
 	_, err := NewCarbonInterval(startDate, endDate)
-	assert.Error(t, err, "The end date must be greater than the start date.")
+	assert.Error(t, err, "the end date must be greater than or equal to the start date")
 }
 
 func TestDiffInHours(t *testing.T) {
@@ -20,4 +21,23 @@ func TestDiffInHours(t *testing.T) {
 
 	interval, _ := NewCarbonInterval(startDate, endDate)
 	assert.Equal(t, int64(1), interval.DiffInHours())
+}
+
+func TestErrorOnNilStartDate(t *testing.T) {
+	endDate, _ := Create(2009, time.November, 10, 24, 0, 0, 0, "UTC")
+
+	_, err := NewCarbonInterval(nil, endDate)
+	assert.Error(t, err, "start date cannot be nil")
+}
+
+func TestErrorOnNilEndDate(t *testing.T) {
+	startDate, _ := Create(2009, time.November, 10, 23, 0, 0, 0, "UTC")
+
+	_, err := NewCarbonInterval(startDate, nil)
+	assert.Error(t, err, "end date cannot be nil")
+}
+
+func TestErrorOnNilDates(t *testing.T) {
+	_, err := NewCarbonInterval(nil, nil)
+	assert.Error(t, err, "start date cannot be nil")
 }
